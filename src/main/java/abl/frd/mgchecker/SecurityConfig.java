872dca20b4e1fe99250ce4165360651d9e58feff
Tanvir-Ahmed-67 <tanvirahmed67@yahoo.com>
2026-02-15 17:21:43 +0600

    Login credential taken from code instead of properties file.

--- src/main/java/abl/frd/mgchecker/SecurityConfig.java
+++ src/main/java/abl/frd/mgchecker/SecurityConfig.java
@@ -1,9 +1,13 @@
 package abl.frd.mgchecker;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -34,5 +38,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
                 .permitAll();
+    }
+    @Bean
+    public org.springframework.security.core.userdetails.UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = org.springframework.security.core.userdetails.User.withUsername("admin")
+                .password(passwordEncoder.encode("M@g#123"))  // encode password
+                .roles("Admin")
+                .build();
+        return new org.springframework.security.provisioning.InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
